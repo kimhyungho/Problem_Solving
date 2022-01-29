d0cba561b6323cfeea62340d82dc6c3e26dad954
kimhyungho <rla88175@gmail.com>
2022-01-29 20:05:21 +0900

    2022.1.29

--- chapter5/문제5M.와일드 카드/5m.java
+++ chapter5/문제5M.와일드 카드/5m.java
@@ -1,8 +1,39 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
 class Main {
 
-    public static void main(String[] args) {
-  
-    }
+	public static final Scanner scanner = new Scanner(System.in);
+	
+  public static void main(String[] args) {
+		int n = scanner.nextInt();
+		String whildWord = scanner.next();
+		
+		for(int i = 0; i < n; i += 1) {
+ 			Boolean isSame = true;
+
+
+			String word = scanner.next();
+			
+			if(word.length() != whildWord.length()) {
+				continue;
+			}
+			
+			for(int index = 0; index < word.length(); index += 1) {
+				if(whildWord.charAt(index) != word.charAt(index)) {
+					if(whildWord.charAt(index) == '?'){
+						continue;
+					} else {
+						isSame = false;
+						break;
+					}
+				}
+			}
+			
+			if(isSame == true) {
+				System.out.println(word);
+			}
+		}
   }
-  
-  
+}
