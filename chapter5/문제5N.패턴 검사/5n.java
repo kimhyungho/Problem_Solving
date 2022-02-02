9ce498406e8f0f3e3adc12193e4bfe25e2c305c1
kimhyungho <rla88175@gmail.com>
2022-02-02 18:51:13 +0900

    2022.2.2

--- chapter5/문제5N.패턴 검사/5n.java
+++ chapter5/문제5N.패턴 검사/5n.java
@@ -1,6 +1,17 @@
-class Main {
+import java.lang.*;
+import java.util.*;
 
-    public static void main(String[] args) {
-          
-    }
-  }  
+class Main {
+	public static final Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+		String s = scanner.next();
+		String p = scanner.next();
+		
+		for(int i = 0; i < s.length() - p.length(); i ++) {
+			String d = s.substring(i, i + p.length());
+			if(d.equals(p)) {
+				System.out.println(i);
+			}
+		}
+  }
+}
